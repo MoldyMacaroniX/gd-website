db550cfdb4cb49fe8acf63fb5fb2c9fe6210048a
MoldyMacaroniX <moldymacaronix@users.noreply.github.com>
2022-02-06 23:26:54 -0500

    New articles / authors

--- src/_data/site.js
+++ src/_data/site.js
@@ -1,6 +1,6 @@
 module.exports = {
     title: 'Dashword',
-    desc: 'The goal for Dashword is to share some of the endless stories this game has to offer, from help guides to in-depth player experiences.',
+    desc: 'The goal for Dashword is to share some of the endless stories Geometry Dash has to offer, from help guides to in-depth player experiences.',
     icon: '/assets/img/icon.png',
     discordinvite: 'https://discord.gg/SqZuGCpHMm',
     image: 'https://i.ytimg.com/vi/ipK7vQ8gEZw/maxresdefault.jpg',
